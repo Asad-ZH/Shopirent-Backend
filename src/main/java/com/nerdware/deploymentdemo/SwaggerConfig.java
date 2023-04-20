85ce7c2d6e2fe00567554569d0c63467b6a48d5f
asad <asadzahid1479@gmail.com>
2023-04-21 03:48:45 +0500

    bug resolved

--- src/main/java/com/nerdware/deploymentdemo/SwaggerConfig.java
+++ src/main/java/com/nerdware/deploymentdemo/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .apis(RequestHandlerSelectors.basePackage("com.nerdware.deploymentdemo"))
                 .paths(PathSelectors.any())
                 .build()
                 .securitySchemes(Collections.singletonList(apiKey()))
